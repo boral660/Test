e20b6db8272aadc41a6af9102457ddaadc5cff13
Alex-ПК\Alex <boral660@gmail.com>
2017-10-12 19:29:46 +0300

    DSD`

--- IAnalysisMethod.cs
+++ IAnalysisMethod.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//test
+//testы
 namespace Steganography.Analysis
 {
     public interface IAnalysisMethod
